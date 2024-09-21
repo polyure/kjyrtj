ffeac199a4afbf9706cf151c9267f07303630665
Tommy Johansson <tommy.johansson@gmail.com>
2024-09-21 19:59:03 +0300

    First commit

---
+++ kjyrtj.py
@@ -0,0 +1,44 @@
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from datetime import datetime
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+TOKEN = os.getenv("TOKEN")
+
+KJYR_DATE = datetime(2024, 10, 16, 17, 0, 0)
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Olen KJYR-tj-botti.")
+
+async def count_days(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    tj = KJYR_DATE- datetime.now()
+    tj = str(tj.days)
+    text = "KJYR-TJ: " + tj
+    await update.effective_message.reply_text(text)
+    
+    
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(TOKEN).build()
+    
+    start_handler = CommandHandler('start', start)
+    
+    application.add_handler(start_handler)
+    application.add_handler(CommandHandler("kjyrtj", count_days))
+    application.add_handler(CommandHandler("tj", count_days))
+    application.add_handler(CommandHandler("tjkjyr", count_days))
+    application.add_handler(CommandHandler("tillkjyr", count_days))
+    application.add_handler(CommandHandler("countdayskjyr", count_days))
+    application.add_handler(CommandHandler("paiviakunneskjyr", count_days))
+    application.add_handler(CommandHandler("kjyriimme", count_days))
+    application.add_handler(CommandHandler("kjyrcountdown", count_days))
+    application.add_handler(CommandHandler("KauankoJaljellaYhteisRisteilyyn", count_days))
+    
+    application.run_polling()
